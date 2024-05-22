2dafb92299b0e4e56ba172ebf364c83f2feedaa8
RachyHassan <rachyhassan@github.com>
2024-05-22 13:40:49 +0100

    task_1

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import logo from './Holberton-Logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy} from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="Holberton-Logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
