7a54272c3f115c61bbf3ca2a904367cf03e16045
RachyHassan <rachyhassan@github.com>
2024-05-22 13:46:25 +0100

    task_2

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -10,6 +10,11 @@ function App() {
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
       </div>
       <footer className="App-footer">
         <p>Copyright 2020 - holberton School</p>
